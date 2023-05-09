385ec4d111816fea35d9fedafecf113fd0e8f5b7
Cyleng <lengchuanyu@outlook.com>
2023-05-09 15:59:17 -0500

    init update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./conponents/Button/button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button disabled>Hello</Button>
+        <Button>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Hello
+        </Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com">
+          Hello
+        </Button>
+
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+          Hello Danger
+        </Button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
